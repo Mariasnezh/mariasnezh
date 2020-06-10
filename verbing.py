644405381fd90bf7e7e614e5c3caa5da28d005fb
Mariasnezh <61145056+Mariasnezh@users.noreply.github.com>
2020-06-10 11:34:47 +0300

    Update verbing.py

--- verbing.py
+++ verbing.py
@@ -6,3 +6,14 @@ elif not b:
     print(a + 'ing')
 elif b:
     print(a.replace('ing', 'ly'))
+    
+#второй вариант:    
+    
+a = str(input())
+b = a.endswith('ing')
+if len(a) < 3:
+    print(a)
+elif not b:
+    print(a + 'ing')
+else:
+    print(a.replace('ing', 'ly'))
