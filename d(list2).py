4b25a8b6904bfbd85e414ea431a20ca7cea200cf
Mariasnezh <61145056+Mariasnezh@users.noreply.github.com>
2020-06-20 00:59:59 +0300

    Update d(list2).py

--- d(list2).py
+++ d(list2).py
@@ -18,3 +18,23 @@ while i < len(list):
 print(list2)
 
 #сама не знаю как это получилось, но вроде работает
+#второй вариант:
+
+list = []
+list2 = []
+emp = ""
+
+while True:
+    line = input()
+    if line.strip() == emp:
+        break
+    list.append(int(line))
+
+print(list)
+
+i = 0
+while i < len(list):
+    if list[i] != list[i-1]:
+        list2.append(list[i])
+    i += 1
+print(list2)
