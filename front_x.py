27669d31c207a027388c51b6a00b145c8156059e
Mariasnezh <61145056+Mariasnezh@users.noreply.github.com>
2020-06-30 12:41:03 +0300

    Update front_x.py

--- front_x.py
+++ front_x.py
@@ -19,3 +19,11 @@ list3 = list1 + list2
 #print(list1)
 #print(list2)
 print(list3)
+
+# B. front_x
+# Given a list of strings, return a list with the strings
+# in sorted order, except group all the strings that begin with 'x' first.
+# e.g. ['mix', 'xyz', 'apple', 'xanadu', 'aardvark'] yields
+# ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
+# Hint: this can be done by making 2 lists and sorting each of them
+# before combining them.
